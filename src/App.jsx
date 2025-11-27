cca52f99a030fd5eafcf9c246a3f2b879ef192e4
aoclems <aoclems@gmail.com>
2025-11-27 12:28:32 +0100

    New UI v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./App.css";
+
 
 function App() {
   const navigate = useNavigate();
