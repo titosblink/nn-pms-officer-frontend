a76ce8e1a616d5f86a7cb3408a762a6e50fd680d
aoclems <aoclems@gmail.com>
2025-12-24 10:01:44 +0100

    24 Dec 25 v88

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 import Sidenav from "./Sidenav";
-import Sidenav from "./Header";
 
 export default function Dashboard() {
     const navigate = useNavigate();
@@ -46,7 +45,29 @@ export default function Dashboard() {
             <div className="bg-base-200 flex min-h-screen flex-col">
                 {/* Layout Navbar */}
                 {/* ---------- HEADER ---------- */}
-                < Header/>
+                <div className="bg-base-100 border-base-content/20 lg:ps-75 sticky top-0 z-50 flex border-b">
+                    <div className="mx-auto w-full max-w-7xl">
+                        <nav className="navbar py-2">
+                            <div className="navbar-start items-center gap-2">
+                                <button type="button" className="btn btn-soft btn-square btn-sm lg:hidden" aria-haspopup="dialog" aria-expanded="false" aria-controls="layout-sidebar" data-overlay="#layout-sidebar">
+                                    <span className="icon-[tabler--menu-2] size-4.5" />
+                                </button>
+                                {/* Search  */}
+                                <div className="input no-focus border-0 px-0">
+                                    <span className="icon-[tabler--search] text-base-content/80 my-auto me-2 size-4 shrink-0" />
+                                    <input type="search" className="grow placeholder:text-sm" placeholder="Type to Search..." id="kbdInput" />
+                                    <label className="sr-only" htmlFor="kbdInput">Search</label>
+                                </div>
+                            </div>
+                            <div className="navbar-end items-end gap-6">
+                                {/* GitHub Button */}
+                                <div className="max-md:hidden">
+                                    <Link className="github-button" href="#" data-icon="octicon-star" data-size="large" data-show-count="true">Welcome</Link>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
                 {/* Menu */}
                 < Sidenav/>
                 {/* / Menu */}
